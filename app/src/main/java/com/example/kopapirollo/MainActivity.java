1ee08860dc5854fbfc0e0294521a22282ad037db
GNorbeee <gyulai.norbert@gmail.com>
2020-10-16 17:57:47 +0200

    Kinezet kesz, mar csak a kodolas van hatra

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.kopapirollo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int geperedmeny, eneredmeny, valasztas;
+    private Button btnKo, btnPapir, btnOllo;
+    private TextView gepvegeredmeny, envegeredmeny;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnKo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valasztas=0;
+
+            }
+        });
+        btnPapir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valasztas=1;
+            }
+        });
+        btnOllo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valasztas=2;
+            }
+        });
+
     }
 }
